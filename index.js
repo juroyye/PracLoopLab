a8f64012cae0b1d59a3512ad814db4d6efaa83ae
Justin M <manningjustin08@yahoo.com>
2024-09-24 11:14:40 -0400

    Completed lab

---
+++ index.js
@@ -0,0 +1,21 @@
+for (let i = 1; i <= 100; i++){
+    if (i % 15 == 0){
+        console.log("Fizz Buzz")
+    } else if (i % 3 == 0){
+        console.log("Fizz")
+    } else if (i % 5 == 0){
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
+
+
+for(let i = 1; i < 100;++i)console.log((i%3?'':'Fizz') + (i%5?'':'Buzz') || i)
+
+////////////////////////////////////
+
+
+
+
+
